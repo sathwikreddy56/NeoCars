a21ce5398d154942c9133b374ac38b3b48a55199
unknown <abhiramnellutla1504@gmail.com>
2021-04-03 12:06:52 +0530

    front-end basic folder format@abhiram

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,10 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminSideComponent } from './admin-side/admin-side.component';
+import { AdminDashboardComponent } from './admin-side/admin-dashboard/admin-dashboard.component';
+import { AdminNavbarComponent } from './admin-side/admin-navbar/admin-navbar.component';
+import { AdminProfileComponent } from './admin-side/admin-profile/admin-profile.component';
+import { UserSideComponent } from './user-side/user-side.component';
+import { UserDashboardComponent } from './user-side/user-dashboard/user-dashboard.component';
+import { UserNavbarComponent } from './user-side/user-navbar/user-navbar.component';
+import { UserProfileComponent } from './user-side/user-profile/user-profile.component';
+import { SuperAdminSideComponent } from './super-admin-side/super-admin-side.component';
+import { SuperAdminDashboardComponent } from './super-admin-side/super-admin-dashboard/super-admin-dashboard.component';
+import { SuperAdminNavbarComponent } from './super-admin-side/super-admin-navbar/super-admin-navbar.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    AdminSideComponent,
+    AdminDashboardComponent,
+    AdminNavbarComponent,
+    AdminProfileComponent,
+    UserSideComponent,
+    UserDashboardComponent,
+    UserNavbarComponent,
+    UserProfileComponent,
+    SuperAdminSideComponent,
+    SuperAdminDashboardComponent,
+    SuperAdminNavbarComponent,
+    LoginComponent,
+    SignupComponent,
    
   ],
   imports: [
---
+++ angular/src/app/super-admin-side/super-admin-navbar/super-admin-navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-super-admin-navbar',
+  templateUrl: './super-admin-navbar.component.html',
+  styleUrls: ['./super-admin-navbar.component.css']
+})
+export class SuperAdminNavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
